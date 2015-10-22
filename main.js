7012f63629d43b7365c305a24852f095caa1730b
thyagostall <thstall@gmail.com>
2015-10-22 14:41:51 -0200

    Correção dos 3 problemas

--- main.js
+++ main.js
@@ -1,22 +1,16 @@
-$( document ).ready(function() {
-    
-
-	$('#campo-nome').keyup(function(){
-
+$(document).ready(function() {
+	$('#campo-nome').keyup(function() {
 		// Obter o nome digitado e o atribuir na variável abaixo
-		var nomeDigitado = '';
+		var nomeDigitado = $('#campo-nome').val();
 
-		// O ID nome-digitado deveria estar recebendo o nome digitado pelo usuário, isto não está acontecendo, 
+		// O ID nome-digitado deveria estar recebendo o nome digitado pelo usuário, isto não está acontecendo,
 		// verifique o log de erros no navegador e o corrija.
-		$('#nome-digitado').text(nomedigitado);
-
+		$('#nome-digitado').text(nomeDigitado);
 
 		// A variável abaixo deve conter a quantidade de caracteres do nome digitado, crie a lógica para obter este valor
-		var quantidadeCaracteres = 0;
-
+		var quantidadeCaracteres = nomeDigitado.length;
 
 		// Em seguida informe este valor no elementdo com ID quantidade-caracteres
-
+    $('#quantidade-caracteres').text(quantidadeCaracteres);
 	});
-
 });
